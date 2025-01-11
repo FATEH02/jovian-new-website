94015875054e4e9ac493d15ad2f62da21c421c01
Fateh Singh <fatehsingh07462@gmail.com>
2025-01-11 06:29:08 +0000

    new updates

--- app.py
+++ app.py
@@ -1,51 +1,26 @@
-from flask import Flask, json , render_template , jsonify
-from dataabas import engine
+from flask import Flask, json, render_template, jsonify
+from dataabase import engine
 from sqlalchemy import text
 
 app = Flask(__name__)
-# simuating database here we can pass this data in our html file
-# JOBS=[{
-#   'id':1,
-#   'title':'Data Analyst',
-#   'location':'Bangluru,India',
-#   'salary':'10,00,000'
-# },
-# {
-#   'id':2,
-#   'title':'Data Analyst',
-#   'location':'Delhi,India',
-#   'salary':'1,00,000'
-# },
-#  {
-#   'id':3,
-#   'title':'Front end Engineer',
-#   'location':'Remote',
-#   # 'salary':'7,00,000'
-#  },
-# {
-#   'id':4,
-#   'title':'Back End Engineer',
-#   'location':'San Francisco,USA',
-#   'salary':'$10000'
-# }      
-#      ]
 
 def load_jobs_from_db():
-  with engine.connect() as connection:
-    print("Connected to the database!")
-    result = connection.execute(text("SELECT * FROM jobs"))
-    result_dict = [dict(row._mapping) for row in result]
-    print(result_dict)
-
+    with engine.connect() as connection:
+        print("Connected to the database!")
+        result = connection.execute(text("SELECT * FROM jobs"))
+        result_dict = [dict(row._mapping) for row in result]
+        print(result_dict)
+        return result_dict  # Add this line to return the fetched jobs
 
 @app.route("/")
 def hello_world():
-  jobs=load_jobs_from_db()
-  return render_template('home.html',jobs=jobs)  
+    jobs = load_jobs_from_db()  # Now it will have the data
+    return render_template('home    .html', jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+    jobs = load_jobs_from_db()  # Now it will have the data
+    return jsonify(jobs)
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0', debug=True) 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
